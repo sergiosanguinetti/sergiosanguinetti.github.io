d4bf5c669da7920c268ac4384755bb8262cc2178
sergiosanguinetti <sergiosanguinetti@hotmail.com>
2023-02-26 16:12:34 -0300

    primer web

---
+++ js/funciones.js
@@ -0,0 +1,7 @@
+document.getElementById('botonwhat').onclick = function () {
+    document.getElementById("whatsapp").innerHTML="1231546" ;
+}
+
+document.getElementById('botonemail').onclick = function () {
+    document.getElementById("email").innerHTML="sergio@hotmail.com" ;
+}
